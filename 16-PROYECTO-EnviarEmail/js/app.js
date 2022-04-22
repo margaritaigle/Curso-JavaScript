4872e06efcb4fecc8b796df5ef9f5d4344038dd2
Margarita Iglesias <margaritaigle99@gmail.com>
2022-04-21 22:06:48 -0300

    hasta 128 terminado

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,53 @@
+// Variables
+const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
+
+// Variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
+// Event listener
+eventListeners();
+
+function eventListeners() {
+    // Cuando la app arranca
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+    
+    // Campos del formulario
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
+}
+
+
+
+// Funciones
+
+function iniciarApp() {
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50')
+}
+
+// Valida formulario
+function validarFormulario(e) {
+    if(e.target.value.length > 0) {
+        console.log('Si hay algo');
+    } else {
+        e.target.classList.add('border', 'border-red-500');
+
+        mostrarError();
+    }
+}
+
+function mostrarError() {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('border', 'border-red-500', 'background-color-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+    
+    const errores = document.querySelectorAll('.error');
+    if(errores.length === 0) {
+        formulario.appendChild(mensajeError);
+    }
+    
+}
